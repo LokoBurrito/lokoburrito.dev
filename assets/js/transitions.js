1e1846adaff5b188bfe7d82fa86ac6313aec1462
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2026-01-04 23:44:56 -0800

    Add page transition and enter animations

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -1,1 +1,18 @@
+export function pageTransition() {
+  const tl = gsap.timeline();
+  tl.to("main", {
+    opacity: 0,
+    duration: 0.4,
+    ease: "power1.out"
+  });
+  return tl;
+}
 
+export function pageEnter() {
+  gsap.from("main", {
+    opacity: 0,
+    y: 20,
+    duration: 0.6,
+    ease: "power2.out"
+  });
+}
