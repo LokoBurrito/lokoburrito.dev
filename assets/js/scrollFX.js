0f3634b6730400d693d6437c7289728e1b152e65
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2026-01-04 23:45:34 -0800

    Add scrollTitles function for section animations

--- assets/js/scrollFX.js
+++ assets/js/scrollFX.js
@@ -1,1 +1,13 @@
-
+export function scrollTitles() {
+  document.querySelectorAll("section").forEach(sec => {
+    gsap.from(sec.querySelector("h2"), {
+      scrollTrigger: {
+        trigger: sec,
+        start: "top 80%"
+      },
+      y: 40,
+      opacity: 0,
+      duration: 0.6
+    });
+  });
+}
