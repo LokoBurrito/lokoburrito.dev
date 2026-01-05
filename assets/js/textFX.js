7695fa8ed13c9fec0aad1978080ab006e8c8e40f
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2026-01-04 23:45:23 -0800

    Add animateText function for text animation

--- assets/js/textFX.js
+++ assets/js/textFX.js
@@ -1,1 +1,21 @@
+export function animateText() {
+  document.querySelectorAll(".fx-text").forEach(el => {
+    const text = el.innerText;
+    el.innerHTML = "";
 
+    [...text].forEach((char, i) => {
+      const span = document.createElement("span");
+      span.innerText = char === " " ? "\u00A0" : char;
+      span.style.opacity = 0;
+      el.appendChild(span);
+
+      gsap.to(span, {
+        opacity: 1,
+        y: 0,
+        delay: i * 0.05,
+        duration: 0.4,
+        ease: "power2.out"
+      });
+    });
+  });
+}
