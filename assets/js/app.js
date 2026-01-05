336ed6ae01876264355f96bade25540536c8187b
LokoBurrito <81943357+LokoBurrito@users.noreply.github.com>
2026-01-04 23:45:10 -0800

    Add page transitions and text animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
+import { pageTransition, pageEnter } from "./transitions.js";
+import { animateText } from "./textFX.js";
 
+barba.init({
+  transitions: [
+    {
+      leave() {
+        return pageTransition();
+      },
+      enter() {
+        pageEnter();
+        animateText();
+      }
+    }
+  ]
+});
+
+window.addEventListener("load", animateText);
